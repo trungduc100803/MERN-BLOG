51cc93a96b199f5730164e0fe89dd93525094d7c
trungduc100803 <duct5732@gmail.com>
2024-04-28 22:12:46 +0700

    function showmore post

--- client/src/components/DashboardPost.jsx
+++ client/src/components/DashboardPost.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom"
 const DashboardPost = () => {
   const {currentUser} = useSelector(state => state.user)
   const [userPosts, setUserPosts] = useState([])
+  const [showMore, setShowMore] = useState(true)
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -14,6 +15,9 @@ const DashboardPost = () => {
         const data = await res.json()
         if(res.ok){
           setUserPosts(data.posts)
+          if(data.post.length < 9){
+            setShowMore(false)
+          }
         }
       } catch (error) {
         console.log(error.message)
@@ -24,7 +28,23 @@ const DashboardPost = () => {
     }
   }, [currentUser._id])
 
-  console.log(userPosts)
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length
+    try {
+      const res = await fetch(`/api/post/getpost?userID=${currentUser._id}&startIndex=${startIndex}`)
+      const data = await res.json()
+
+      if(res.ok){
+        setUserPosts(prev => [...prev, ...data.posts])
+        if(data.posts.length < 9){
+          setShowMore(false)
+        }
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       { 
@@ -69,6 +89,11 @@ const DashboardPost = () => {
               ))
             }
           </Table>
+          {
+            showMore &&(
+              <button  onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">Show more</button>
+            )
+          }
           </>
         ) : (
           <p>You have no posts yet!!</p>
