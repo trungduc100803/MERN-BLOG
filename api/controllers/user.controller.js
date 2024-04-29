e0cae3aa9e23d4639f4ef3e87c4deec6331b1798
trungduc100803 <duct5732@gmail.com>
2024-04-29 11:27:35 +0700

    get users api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -71,6 +71,47 @@ const userController = {
         } catch (error) {
             next(error)
         }
+    },
+    getUser: async (req, res, next) => {
+        if(!req.user.isAdmin){
+            return next(errorHandler(403, "You are not allowed to see all users"))
+        }
+
+        try {
+            const startIndex = parseInt(req.query.startIndex) || 0
+            const limit = parseInt(req.query.limit) || 9
+            const sortDirection = req.query.sort === "asc" ? 1 : -1
+
+            const users = await User.find()
+                .sort({createdAt: sortDirection})
+                .skip(startIndex)
+                .limit(limit)
+
+            const userWithoutPassword = users.map(user => {
+                const {password, ...rest} = user._doc
+                return rest
+            })
+            const totalUser = await User.countDocuments()
+            const now = new Date()
+            const oneMonthAgo = new Date(
+                now.getFullYear(),
+                now.getMonth() -1,
+                now.getDate()
+            )
+            const lastMonthUser = await User.countDocuments({
+                createdAt: {$gte: oneMonthAgo}
+            })
+
+            return res.status(200).send({
+                users: userWithoutPassword,
+                totalUser,
+                lastMonthUser,
+                success: true,
+                message: 'get user successfully'
+            })
+        } catch (error) {
+            next(error)
+        }
     }
 }
 
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -9,6 +9,6 @@ import userController from '../controllers/user.controller.js';
 router.put('/update/:userID',verifyToken, userController.updateUser)
 router.delete('/delete/:userID',verifyToken, userController.deleteUser)
 router.post('/signout', userController.signOut)
-
+router.get('/getusers', verifyToken, userController.getUser)
 
 export default router
