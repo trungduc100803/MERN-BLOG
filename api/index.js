e477fad8fb1156531aefe7b98b8aabe4aa519a0e
trungduc100803 <duct5732@gmail.com>
2024-05-19 13:09:36 +0700

    add prev deploy

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import cookie_parser from 'cookie-parser'
+import path from 'path'
+
 
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
@@ -27,11 +29,19 @@ mongoose
         console.log(err)
     })
 
+const __dirname = path.resolve()
+
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/post', postRouter)
 app.use('/api/comment', commentRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
